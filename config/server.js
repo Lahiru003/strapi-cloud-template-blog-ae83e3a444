f1fc0da8d03046cbb831f9c9b963ea8d1ae17df8
Lahiru003 <lahirugtx09@gmail.com>
2025-08-18 13:26:34 -0400

    services and price tier chsanges

--- config/server.js
+++ config/server.js
@@ -2,7 +2,10 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS') || [
+      'fd8b8a6a2d3f4e5c6a7b8c9d0e1f2a3b',
+      '9e7d6c5b4a3f2e1d0c9b8a7f6e5d4c3b'
+    ],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
